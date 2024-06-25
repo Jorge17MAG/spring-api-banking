80642798b48ac1c6164a177327132afbf99d6993
Jorge Arellano <jorgemathi4s@gmail.com>
2024-06-25 10:42:39 -0500

    Versión Inicial

---
+++ src/main/java/com/banking/controller/AccountController.java
@@ -0,0 +1,69 @@
+package com.banking.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.banking.model.Account;
+import com.banking.service.AccountService;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+    
+    @Autowired
+    private AccountService accountService;
+
+    @GetMapping
+    private List<Account> getAllAccounts() {
+        return accountService.getAllAccounts();
+    }
+
+    @GetMapping("/{id}")
+    private Account getAccountById(@PathVariable Long id) {
+        return accountService.getAccountById(id);
+    }
+
+    @PostMapping
+    private ResponseEntity<Account> saveAccount(@RequestBody Account account) {
+        Account newAccount = accountService.saveAccount(account);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newAccount);
+    }
+
+    @PutMapping("/{id}")
+    private ResponseEntity<?> updateAccount(@PathVariable Long id, @RequestBody Account account) {
+
+        Account existingAccount = accountService.getAccountById(id);
+
+        if (existingAccount != null) {
+            existingAccount.setAccountHolderName(account.getAccountHolderName());
+            existingAccount.setBalance(account.getBalance());
+            accountService.saveAccount(existingAccount);
+            return ResponseEntity.ok(existingAccount);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cuenta con ID " + id + " no encontrada");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    private ResponseEntity<?> deleteAccount(@PathVariable Long id) {
+        Account existingAccount = accountService.getAccountById(id); 
+
+        if (existingAccount != null) {
+            accountService.deleteAccount(id);
+            return ResponseEntity.ok().body("Eliminado correctamente");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cuenta con ID " + id + " no encontrada");
+        }
+    }
+}
